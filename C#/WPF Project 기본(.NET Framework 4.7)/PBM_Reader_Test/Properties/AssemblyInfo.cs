b936814d33c80ff15e8a62ca195ccbcdf638d7ed
박주형 <juhyung.park@mirero.co.kr>
2023-06-13 08:45:39 +0900

    Add C#

---
+++ C#/WPF Project 기본(.NET Framework 4.7)/PBM_Reader_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PBM_Reader_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PBM_Reader_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f0ca497-65fe-486b-a0fd-523366145a40")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
